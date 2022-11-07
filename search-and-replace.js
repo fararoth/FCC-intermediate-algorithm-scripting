33cb2580bbd002f4fe3abe047c62ebdea2ee8c15
Luis Farias <luisfariasmusicista@gmail.com>
2022-11-07 16:06:25 -0400

    Init search and replace

---
+++ search-and-replace.js
@@ -0,0 +1,9 @@
+function myReplace(str, before, after) {
+
+    str = str.split(" ");
+
+    return str;
+  }
+  
+  /*debug test*/
+  myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
