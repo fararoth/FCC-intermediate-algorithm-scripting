90b4c83813d321c5c3a8bc863cd270ec5ff3f5b6
Luis Farias <luisfariasmusicista@gmail.com>
2022-11-06 20:42:13 -0400

    Finish sum all primes

--- sum-all-primes.js
+++ sum-all-primes.js
@@ -1,10 +1,33 @@
+function primeVerify(num) {
+
+  if( num == 0 || num == 1 || num == 4){
+    return false;
+  }
+      
+  for(var i=2;i<=num-1;i++) {
+    if(num % i==0) {
+  
+      return false;
+  
+    }
+  
+  }
+  
+    return true;
+  
+}
+
+
+
 function sumPrimes(num) {
     let aux = [];
 
     for(var i = 1; i <= num; i++){
-        aux.push(i);
+      if(primeVerify(i)) {
+
+      aux.push(i);
+      }
     }
-    return aux
+    num=aux.reduce((a, b) => a + b, 0);
+    return num;
   }
-/*test*/
-sumPrimes(10);
